d4f529abbcb11d58757b8697d5a47e12353c081d
Josmar Saiefert <josmar.saiefert@gmail.com>
2018-05-06 19:07:04 -0400

    Tela

---
+++ app/src/main/java/com/example/josmar/calculadora/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.josmar.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void numeros(View view) {
+    }
+
+    public void operacao(View view) {
+    }
+}
